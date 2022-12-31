18f77e111e5d3815d9ef1c42ce5d5dbd291d3f94
Maciej Makara <maciejmakarakontakt@gmail.com>
2022-12-31 13:46:19 +0100

    List11 ex1

---
+++ 11/8/List8/List8/Controllers/GameController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace List8.Controllers
+{
+    public class GameController : Controller
+    {
+  
+        private static readonly Random random = new Random();
+        public static int min { get; set; } = 0;
+
+        //public static int randValue { get; set; } = 0;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Set(int n)
+        {
+            //max = n;
+            HttpContext.Session.SetInt32("max", n);
+            ViewBag.Min = ""+min;
+            ViewBag.Max = "" + n;
+            return View("Set");
+        }
+        public IActionResult Guess(int x)
+        {
+            int counter = (int)HttpContext.Session.GetInt32("counter");
+            counter++;
+            int randValue = (int)HttpContext.Session.GetInt32("randValue");
+            HttpContext.Session.SetInt32("counter", counter);
+            ViewBag.Counter = counter;
+            ViewBag.X = x;
+            ViewBag.Min = "" + min;
+            int max = (int)HttpContext.Session.GetInt32("max");
+            ViewBag.Max = "" + max;
+            if (x < min)
+            {
+                ViewBag.Message = "Ponizej minimum";
+                ViewBag.type = "zero";
+            }
+            else if (x > max)
+            {
+                ViewBag.Message = "Powyzej maximum";
+                ViewBag.type = "zero";
+            }
+            else if (x > randValue)
+            {
+                ViewBag.Message = "Powyzej szukanej";
+                ViewBag.type = "multi";
+            }
+            else if (x < randValue)
+            {
+                ViewBag.Message = "Ponizej szukanej";
+                ViewBag.type = "two";
+            }
+            else if (x == randValue)
+            {
+                ViewBag.Message = "Zgadles";
+                ViewBag.type = "one";
+            }
+            
+            return View("Guess");
+        }
+        public IActionResult Draw()
+        {
+            int randValue = random.Next(min, (int)HttpContext.Session.GetInt32("max"));
+            HttpContext.Session.SetInt32("randValue", randValue);
+            HttpContext.Session.SetInt32("counter", 0);
+            ViewBag.Min = "" + min;
+            ViewBag.Max = "" + (int)HttpContext.Session.GetInt32("max");
+            return View("Draw");
+        }
+    }
+}
---
+++ 11/8/List8/List8/Controllers/ToolController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace List8.Controllers
+{
+    public class ToolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public static List<double> GetSolutions(double a, double b, double c)
+		{
+
+			List<double> solutions = new List<double>();
+			if (a == 0)
+			{
+				if (b != 0)
+					solutions.Add(-c / b);
+				else
+				{
+					if (c == 0)
+					{
+						solutions.Add(0);
+						solutions.Add(0);
+						solutions.Add(0);
+					}
+				}
+			}
+			else
+			{
+				double delta = (b * b) - 4 * a * c;
+
+				if (delta == 0)
+				{
+					solutions.Add(-b / (2 * a));
+				}
+				else if (delta > 0)
+				{
+					solutions.Add((-b - Math.Sqrt(delta)) / (2 * a));
+					solutions.Add((-b + Math.Sqrt(delta)) / (2 * a));
+				}
+			}
+			return solutions;
+		}
+
+		public static double RoundToSignificantDigits(double d, int digits)
+		{
+			if (d == 0)
+				return 0;
+
+			double scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(d))) + 1);
+			return scale * Math.Round(d / scale, digits);
+		}
+
+		public IActionResult Solve(double a, double b, double c)
+		{
+
+			List<double> solutions = GetSolutions(a, b, c);
+
+
+			List<double> results = new List<double>();
+			foreach (double item in solutions)
+			{
+				results.Add(RoundToSignificantDigits(item, 5));
+			}
+
+			if (results.Count == 2)
+			{
+				ViewBag.Message = $"x1 = {results[0]} x2 = {results[1]}";
+				ViewBag.type = "two";
+			}
+			else if (results.Count == 1)
+			{
+				ViewBag.Message = $"x = {results[0]}";
+				ViewBag.type = "one";
+			}
+			else if (results.Count == 3)
+			{
+				ViewBag.Message = "Nieskoczenie wiele rozw";
+				ViewBag.type = "multi";
+			}
+			else
+			{
+				ViewBag.Message = "Brak rozw";
+				ViewBag.type = "zero";
+			}
+
+			return View("Solve");
+
+		}
+	}
+}
